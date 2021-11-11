c7f1cac8ffafe2e42eaeac11b505cc0d7771cae4
estebanm4 <30636319+estebanm4@users.noreply.github.com>
2021-11-10 19:09:15 -0500

    Delete Punto15.m

--- Punto15.m
+++
@@ -1,9 +0,0 @@
-%% ----------------- Problem 1.5 --------------------------
-
-R = 152.4; % 500ft
-Smin = 5e-13; %Minimum detectable signal
-Ae = 0.0557; %Area of the antenna
-G = 648.005; %Gain in natural units
-sigma = 10; %Radar cross section
-
-Pt = Smin * ((4*pi)^2*R^4)/(G*Ae*sigma); %Calculation of the average power
